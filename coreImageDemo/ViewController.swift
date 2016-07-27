860a6e35b6ceb64ce645c1d05513a05f71ba44c7
Nafisa Rahman <nafisarahman@nafisas-mbp.gateway>
2016-07-27 22:44:31 +1000

    Initial Commit

---
+++ coreImageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coreImageDemo
+//
+//  Created by Nafisa Rahman on 7/27/16.
+//  Copyright © 2016 Nafisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
